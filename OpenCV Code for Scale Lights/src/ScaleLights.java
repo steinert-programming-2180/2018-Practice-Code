804817c8671c0a97693fafa7694f98419853407c
steinertprogramming <steinertprogramming@gmail.com>
2018-01-30 21:54:29 -0500

    add new timer and better feedback

--- OpenCV Code for Scale Lights/src/ScaleLights.java
+++ OpenCV Code for Scale Lights/src/ScaleLights.java
@@ -7,6 +7,7 @@ import java.awt.image.DataBufferByte;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.TimeUnit;
 
 import javax.swing.ImageIcon;
@@ -26,99 +27,122 @@ import org.opencv.imgproc.Moments;
 import org.opencv.videoio.VideoCapture;
 
 public class ScaleLights {
+	static {
+		System.load("C:/Users/Steinert Robotics/Downloads/opencv/build/java/x64/opencv_java340.dll");
+	}
+	
+	static VideoCapture camera = new VideoCapture(0);
+	
+	static Mat frame = new Mat();
+	static Mat hsv = new Mat();
+	static Mat medianBlur = new Mat();
+	static Mat threshold = new Mat();
+	static Mat erode = new Mat();
+	static Mat dilate = new Mat();
+	static Mat element = new Mat();
+//	Mat dest = new Mat();
+	
+	static Size s = new Size();
+	
+	static double morphSize = 2;
+	
+	static Moments p = new Moments();
+	
+	// uncomment when using red LED light
+	static Scalar lowerRed = new Scalar(95, 0, 230); // 2nd parameter (saturation) doesn't matter; old V: 220
+	static Scalar upperRed = new Scalar(180, 240, 240); // 2nd parameter (saturation) doesn't matter
+	
+	static List<MatOfPoint> contours = new ArrayList<>();
+	static List<Moments> mu = new ArrayList<>();
+	
+	static long period = 1000L; // code is run every 100 ms
+	static long delay = 0L; // code starts immediately
 	
 	public static void main(String[] args) {
-		System.load("C:/Users/Steinert Robotics/Downloads/opencv/build/java/x64/opencv_java340.dll");
-		
-		VideoCapture camera = new VideoCapture(0);
-		
-		Mat frame = new Mat();
-		Mat hsv = new Mat();
-		Mat medianBlur = new Mat();
-		Mat threshold = new Mat();
-		Mat erode = new Mat();
-		Mat dilate = new Mat();
-		Mat element = new Mat();
-//		Mat dest = new Mat();
-		
-		Size s = new Size();
-		
-		double morphSize = 2;
-		
-		Moments p = new Moments();
-		
-		// uncomment when using red LED light
-		Scalar lowerRed = new Scalar(95, 0, 230); // 2nd parameter (saturation) doesn't matter; old V: 220
-		Scalar upperRed = new Scalar(245, 240, 240); // 2nd parameter (saturation) doesn't matter
-		
-		List<MatOfPoint> contours = new ArrayList<>();
-		List<Moments> mu = new ArrayList<>();
 		
 //		hsv = Imgcodecs.imread("ledred.png");
 		
-		while (true) {
-			
-			if (camera.isOpened()) {
-				camera.read(frame);
-			}
-			
-			try {
-				TimeUnit.SECONDS.sleep(1);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			
-			Imgproc.cvtColor(frame, hsv, Imgproc.COLOR_RGB2HSV);
-			
-			Imgproc.medianBlur(hsv, medianBlur, 11);
-			
-			element = Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE, new Size(4*morphSize + 1, (2*morphSize)+1), new Point(morphSize, morphSize));
-			
-			Imgproc.erode(medianBlur, erode, element);
-			Imgproc.dilate(erode, dilate, element);
-			
-			Imgproc.erode(dilate, erode, element);
-			Imgproc.dilate(erode, dilate, element);
-			
-			Core.inRange(dilate, lowerRed, upperRed, threshold);
-			
-//			dest = Mat.zeros(threshold.size(), CvType.CV_8UC3);
-			
-			// Find contours
-			Imgproc.findContours(threshold, contours, new Mat(), Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+		TimerTask lightDetection = new TimerTask() {
 
-			// Draw contours in dest Mat
-			Imgproc.drawContours(dilate, contours, -1, new Scalar(255, 255, 255));
-						
-			for (int i = 0; i < contours.size(); i++) {
-		        mu.add(i, Imgproc.moments(contours.get(i), false));
-		        p = mu.get(i);
-		        int x = (int) (p.get_m10() / p.get_m00());
-		        int y = (int) (p.get_m01() / p.get_m00());
-		        Imgproc.circle(frame, new Point(x, y), 4, new Scalar(255,49,0,255));
-		    }
-			
-			s = frame.size();
-			
-			Imgproc.line(frame, new Point(0, s.height / 2), new Point(s.width, s.height / 2), new Scalar(255, 255, 255), 2);
-			
-			if (mu.size() != 0) { // check if nothing is detected
-				if ((mu.get(0).get_m01() / mu.get(0).get_m00()) > s.height / 2) {
-					Imgproc.putText(frame, "Below line", new Point(10, s.height - 50), Core.FONT_HERSHEY_SIMPLEX, 1, new Scalar(0, 0, 255), 4);
-				} else {
-					Imgproc.putText(frame, "Above line", new Point(10, s.height - 50), Core.FONT_HERSHEY_SIMPLEX, 1, new Scalar(255, 0, 0), 4);
+			@Override
+			public void run() {
+				
+				if (camera.isOpened()) {
+					camera.read(frame);
 				}
-			}	
+				
+				Imgproc.cvtColor(frame, hsv, Imgproc.COLOR_RGB2HSV);
+				
+				Imgproc.medianBlur(hsv, medianBlur, 11);
+				
+				element = Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE, new Size(4*morphSize + 1, (2*morphSize)+1), new Point(morphSize, morphSize));
+				
+				Imgproc.erode(medianBlur, erode, element);
+				Imgproc.dilate(erode, dilate, element);
+				
+				Imgproc.erode(dilate, erode, element);
+				Imgproc.dilate(erode, dilate, element);
+				
+				Core.inRange(dilate, lowerRed, upperRed, threshold);
+				
+//				dest = Mat.zeros(threshold.size(), CvType.CV_8UC3);
+				
+				// Find contours
+				Imgproc.findContours(threshold, contours, new Mat(), Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+
+				// Draw contours in dest Mat
+				Imgproc.drawContours(dilate, contours, -1, new Scalar(255, 255, 255));
+				Imgproc.drawContours(frame, contours, -1, new Scalar(255, 255, 255));
+							
+//				for (int i = 0; i < contours.size(); i++) {
+				if (contours.size() > 0) {
+					mu.add(0, Imgproc.moments(contours.get(0), false));
+			        p = mu.get(0);
+			        int x = (int) (p.get_m10() / p.get_m00());
+			        int y = (int) (p.get_m01() / p.get_m00());
+			        System.out.println("X: " + x + "\nY: " + y);
+			        Imgproc.circle(frame, new Point(x, y), 4, new Scalar(255,49,0,255));
+				}
+			        
+//			    }
+				
+				s = frame.size();
+				
+				Imgproc.line(frame, new Point(0, s.height / 2), new Point(s.width, s.height / 2), new Scalar(255, 255, 255), 2);
+				
+				if (mu.size() != 0) { // check if nothing is detected
+					if (((int) (mu.get(0).get_m01() / mu.get(0).get_m00()) <= s.height / 2) && !((int) (mu.get(0).get_m01() / mu.get(0).get_m00()) == 0)) {
+						
+						Imgproc.putText(frame, "Detected: Above line", new Point(10, s.height - 50), Core.FONT_HERSHEY_SIMPLEX, 1, new Scalar(0, 255, 0), 4);
+					} else if (((int) (mu.get(0).get_m01() / mu.get(0).get_m00()) > s.height / 2) && !((int) (mu.get(0).get_m01() / mu.get(0).get_m00()) == 0)) {
+						
+						Imgproc.putText(frame, "Detected: Below line", new Point(10, s.height - 50), Core.FONT_HERSHEY_SIMPLEX, 1, new Scalar(255, 0, 0), 4);
+					} else if (((int) (mu.get(0).get_m01() / mu.get(0).get_m00()) == 0) && ((int) (p.get_m10() / p.get_m00()) == 0)) {
+						
+						Imgproc.putText(frame, "In frame, not detected", new Point(10, s.height - 50), Core.FONT_HERSHEY_SIMPLEX, 1, new Scalar(0, 0, 255), 4);
+					}
+				} else {
+					Imgproc.putText(frame, "No light in frame", new Point(10, s.height - 50), Core.FONT_HERSHEY_SIMPLEX, 1, new Scalar(0, 0, 255), 4);
+				}
+				
+				displayImage(Mat2BufferedImage(frame), dilate);
+				
+				
+//				displayImage(Mat2BufferedImage(frame));
+				
+				contours.clear(); // delete all current contours to prepare for next iteration
+				mu.clear(); // delete all current moments to prepare for next iteration
+			}
 			
-			displayImage(Mat2BufferedImage(frame), dilate);
-			
-			
-//			displayImage(Mat2BufferedImage(frame));
-			
-			contours.clear(); // delete all current contours to prepare for next iteration
-			mu.clear(); // delete all current moments to prepare for next iteration
+		};
+		
+		Timer timer = new Timer("Timer");
+		if (camera.isOpened()) {
+			timer.scheduleAtFixedRate(lightDetection, delay, period);
 		}
 	}
+			
+			
 	
 	public static BufferedImage Mat2BufferedImage(Mat m){
 	    int bufferSize = m.channels()*m.cols()*m.rows();
@@ -131,8 +155,9 @@ public class ScaleLights {
 	}
 
 	public static void displayImage(Image img2, Mat m) {
+		JFrame frame = new JFrame();
 	    ImageIcon icon = new ImageIcon(img2);
-	    JFrame frame = new JFrame();
+//	    JFrame frame = new JFrame();
 	    
 	    frame.addKeyListener(new KeyListener() {
 	        @Override
@@ -157,5 +182,6 @@ public class ScaleLights {
 	    frame.add(lbl);
 	    frame.setVisible(true);
 	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    
 	}
 }
